c9c2f7ba11ec170ed3f65f3c04c20c43b832d53c
ajcoder42 <84202466+ajcoder42@users.noreply.github.com>
2024-07-04 09:25:24 +0530

    methods of creating react app and cleaning files

---
+++ 01_basics/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h2>Hello | Abhishek started revising react</h2>;
+}
+
+export default App;
---
+++ 02_vite-react-app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h2>Hello | react learning with vite-react project</h2>;
+}
+
+export default App;
